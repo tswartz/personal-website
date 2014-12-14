b3053f73fd0f9658df2a1fc86e4b2b5ad02b60f8
tswartz <tswartz92@gmail.com>
2014-12-14 12:41:40 -0500

    Cover photo text fade in

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	$('.cover-photo-text').addClass("loaded");
+
 	$('.nav-bar-item').click(function (e) {
 		var sectionClass = $(e.target).attr('section') + "-section";
         smoothScrollTo(getYPosition(sectionClass));
