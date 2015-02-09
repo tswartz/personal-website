7e1ed2495c61ec7445bc56bf77e4aebe16edd27b
tswartz <tswartz92@gmail.com>
2015-02-09 16:59:07 -0500

    Got vex working finally

--- script.js
+++ script.js
@@ -18,11 +18,6 @@ $(document).ready(function() {
 		smoothScrollTo(0);
 	});
 
-	// set art thumbnail overlay click event -> vex popup
-	$('.art-thumbnail-overlay').click(function (e) {
-		setArtVexPopup(e);
-	});
-
 });
 
 // Determines if in mobile mode
@@ -68,86 +63,6 @@ function navBarItemMouseOut(navBarItem, navBarItemText) {
 	});
 	link.removeClass("fa");
 	link.html(navBarItemText);
-}
-
-// Gets necessary data about art image when thumbnail is clicked 
-// and opens vex popup with full image displayed
-function setArtVexPopup(e) {
-	var target = $(e.target);
-	if (target.prop("tagName").toLowerCase() == 'p') {
-		target = target.parent();
-	}
-	var thumbnail = target.parent().find('img.art-thumbnail');
-	var vexImage = '<img src="' + thumbnail.attr('pic-src') + '"/>';
-	var overlayText = '<div>' + target.text() + ' - ' + thumbnail.attr('art-media') + '</div>';
-	if (thumbnail.hasClass('zoomable')) {
-		overlayText +=  '<div class="zoomable-text">Hover over image to zoom</div>';
-	}
-	openVexPopup(thumbnail, vexImage, overlayText);
-}
-
-// Opens vex popup with full image displayed and
-// sets up zoom for zoomable images
-function openVexPopup(thumbnail, vexImage, overlayText) {
-	vex.open({
-		showCloseButton: true,
-		content: overlayText + '<div class="vex-content-image-container">' + vexImage + '</div>',
-		buttons: [],
-		afterOpen: function () {
-			if (!thumbnail.hasClass('zoomable')) {
-				return;
-			}
-
-			var myImage = new Image();
-			myImage.src = thumbnail.attr('pic-src');
-
-			myImage.onload = function getWidthAndHeight() {
-			   	setArtZoom(thumbnail, this.width, this.height);
-			    return true;
-			};
-			myImage.onerror = function loadFailure() {
-			    console.log("Image failed to load.");
-			    return true;
-			};
-		}
-	});
-}
-
-// Sets up zooming when hovering over full art image
-function setArtZoom(thumbnail, width, height) {
-	var img = $(".vex-content-image-container img");
-	var imgWidth = img.width();
-	var imgHeight = img.height();
-	var widthRatio = width/imgWidth;
-	var heightRatio = height/imgHeight;
-	img.mouseenter(function (e) {
-		var x = (e.offsetX * widthRatio) - imgWidth;
-		var y = (e.offsetY * heightRatio) - imgHeight;
-		var background = 'url("' + thumbnail.attr('pic-src') + '")';
-		var zoomedImg = $('<img class="zoomed-img"/>');					
-		zoomedImg.css({
-			'position': 'absolute',
-			'max-width': '100%',
-			'background': "" + background,
-			'background-position-x': '-' + x +'px',
-			'background-position-y': '-' + y +'px',
-			'width': imgWidth + '',
-			'height': imgHeight + '',
-			'cursor': 'all-scroll'
-		});
-		$('.vex-content-image-container').prepend(zoomedImg);
-		zoomedImg.mousemove(function (e) {
-			var x = (e.offsetX * widthRatio) - imgWidth;
-			var y = (e.offsetY * heightRatio) - imgHeight;
-			zoomedImg.css('background-position-x', "-" + x + "px");
-			zoomedImg.css('background-position-y', "-" + y + "px");
-		});
-		zoomedImg.mouseleave(function (e) {
-			zoomedImg.remove();
-		});
-	});
-
-	
 }
 
 // gets the y position of a section
