a8b250ca7bb0015d33007412f037b9fa88c5e975
tswartz <tswartz92@gmail.com>
2015-05-05 21:46:58 -0400

    Got angular routing working without hashtags

--- controller.js
+++ controller.js
@@ -4,3 +4,15 @@ app.controller('HomeController', function($scope, $http, $location, $sce)
 {
 
 });
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider.
+    when('/home', {
+        templateUrl: 'about.html'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
+
+    $locationProvider.html5Mode(true);
+}]);
