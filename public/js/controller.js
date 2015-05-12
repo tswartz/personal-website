376d9f21626aa0528bb1f3fd7c0629c649acc76c
tswartz <tswartz92@gmail.com>
2015-05-12 17:09:06 -0400

    Reorganized files and deleted coverphoto

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,10 +2,10 @@ var app = angular.module("TaliaApp", ["ngRoute"]);
 
 app.controller('HomeController', function($scope, $http, $location, $sce)
 {
-	$scope.toggleNavbar = function () {
-		$('.nav-bar').toggleClass('show-nav-bar');
-		$('.nav-icon-container').toggleClass('show-nav-bar');
-	}
+    $scope.toggleNavbar = function () {
+        $('.nav-bar').toggleClass('show-nav-bar');
+        $('.nav-icon-container').toggleClass('show-nav-bar');
+    }
 
     $scope.scrollToTop = function () {
         $('html,body').animate({
@@ -18,20 +18,20 @@ app.controller('HomeController', function($scope, $http, $location, $sce)
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.
     when('/home', {
-        templateUrl: 'pages/home.html',
+        templateUrl: 'pages/home/home.html',
         controller: 'HomeCtrl'
     }).
     when('/about', {
-        templateUrl: 'pages/about.html'
+        templateUrl: 'pages/about/about.html'
     }).
     when('/code', {
-        templateUrl: 'pages/code.html'
+        templateUrl: 'pages/code/code.html'
     }).
     when('/art', {
-        templateUrl: 'pages/art.html'
+        templateUrl: 'pages/art/art.html'
     }).
     when('/contact', {
-        templateUrl: 'pages/contact.html'
+        templateUrl: 'pages/contact/contact.html'
     }).
     otherwise({
         redirectTo: '/home'
